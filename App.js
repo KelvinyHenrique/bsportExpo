e17296318ce17282d93e5a309829d68bd2c4f5b3
Kelviny H <kelvinyhenrique17@gmail.com>
2020-08-30 20:25:02 -0300

    Frist commit

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{width:'100%', height:'100%'}}>
+        <WebView
+        source={{
+          uri: 'https://www.bsport.com.br/'
+        }}
+        style={{marginTop: 20 }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
